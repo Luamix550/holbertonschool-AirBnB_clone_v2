be243cb0e48cabae97f171e8e6bbdba7268f9d02
foultrip <davidvasquezmahecha@gmail.com>
2024-04-11 13:03:39 -0500

    method all dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,20 +6,58 @@ from models.engine import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-class DBStorage():
-        __engine =  None
-        __session = None
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-        def __init__(self):
-                user = os.getenv("HBNB_MYSQL_USER")
-                password = os.getenv("HBNB_MYSQL_PWD")
-                host = os.getenv("HBNB_MYSQL_HOST9", default="localhost")
-                database = os.getenv("HBNB_MYSQL_DB")
 
-                self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                user, password, database), pool_pre_ping=True)
+class DBStorage:
+    __engine = None
+    __session = None
 
-                Base.metadata.create_all(self.__engine)
+    def __init__(self):
+        user = os.getenv("HBNB_MYSQL_USER")
+        password = os.getenv("HBNB_MYSQL_PWD")
+        host = os.getenv("HBNB_MYSQL_HOST9", default="localhost")
+        database = os.getenv("HBNB_MYSQL_DB")
 
-                Session = sessionmaker(bind=self.__engine)
-                session = Session()
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}".format(user, password, database),
+            pool_pre_ping=True,
+        )
+
+        Base.metadata.create_all(self.__engine)
+
+        Session = sessionmaker(bind=self.__engine)
+        session = Session()
+
+    def all(self, cls=None):
+        new_dict = {}
+
+        if cls is None:
+            classes = {
+                "User": User,
+                "Place": Place,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Review": Review,
+            }
+
+            for c in classes:
+                objs = Session.query(c).all()
+
+                for key, value in objs.items():
+                    if value.__class__ == cls:
+                        new_dict[key] = value
+                return new_dict
+        else:
+            objs = self.__session.query(cls).all()
+
+            for key, value in objs:
+                if value.__class__ == cls:
+                    new_dict[key] = value
+            return new_dict
