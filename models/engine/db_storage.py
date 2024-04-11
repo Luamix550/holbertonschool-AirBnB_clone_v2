c5bbed962861dcff3fecb15550e971b03e616b49
Luamix <luizalfonso611@gmail.com>
2024-04-11 14:28:26 -0500

    Fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,31 +5,41 @@ import os
 from models.engine import Base
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session, sessionmaker, scoped_session
+
 class DBStorage():
-        __engine =  None
+
+	__engine =  None
         __session = None
+
         def __init__(self):
                 user = os.getenv("HBNB_MYSQL_USER")
                 password = os.getenv("HBNB_MYSQL_PWD")
                 host = os.getenv("HBNB_MYSQL_HOST9", default="localhost")
                 database = os.getenv("HBNB_MYSQL_DB")
+
                 self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                 user, password, database), pool_pre_ping=True)
+
                 Base.metadata.create_all(self.__engine)
+
                 Session = sessionmaker(bind=self.__engine)
                 session = Session()
+
                 if os.getenv("HBNB_ENV") == "test":
                         Base.metadata.drop_all(self.__engine)
+
                 def new(self, obj):
                         with Session(self.__engine):
                                 session.add(self.__session)
+
                 def save(self):
                         with Session(self.__engine):
                                 session.commit()
+
                 def delete(self, obj=None):
                         if obj is None:
                                 session.delete(obj)
-                
+
                 def reload(self):
                         Base.metadata.create_all(self.__engine)
                         session_factory = sessionmaker(
