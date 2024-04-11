0f8cb10f4a818a41efda5dbb3efcfb5e5a1efcb8
foultrip <davidvasquezmahecha@gmail.com>
2024-04-11 14:21:08 -0500

    fixed model user

--- models/user.py
+++ models/user.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    __tablename__ = "users"
+    __tablename__ = 'users'
     email = Column(String(128), nullable=True)
     password = Column(String(128), nullable=True)
     first_name = Column(String(128), nullable=True)
