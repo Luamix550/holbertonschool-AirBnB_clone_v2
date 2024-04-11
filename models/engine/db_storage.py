9d24c9f5f872097afbb0d4e9c84eb061f82db337
Luamix <luizalfonso611@gmail.com>
2024-04-11 11:52:27 -0500

    Get all cities instances

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import column, string
+from MySQLdb import _mysql
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-class DBStorage(city):
-    
+class DBStorage():
+        __engine =  None
+        __session = None
+
+        user = os.getenv("HBNB_MYSQL_USER")
+        password = os.getenv("HBNB_MYSQL_PWD")
+        host = os.getenv("HBNB_MYSQL_HOST9", default="localhost")
+        database = os.getenv("HBNB_MYSQL_DB")
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        user, password, database), pool_pre_ping=True)
+
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        
