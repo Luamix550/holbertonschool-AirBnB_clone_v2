3703196eb3f7ed9c954e3566422fa6c97958774b
FoulTrip <davidvasquezmahecha@gmail.com>
2024-04-12 14:52:50 -0500

    permission files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,14 @@
 -- Prepares a MySQL server for the project
-CREATE DATABASE hbnb_test_db;
-USE hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_test_db`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
