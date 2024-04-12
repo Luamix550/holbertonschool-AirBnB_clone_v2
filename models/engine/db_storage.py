03442aa1def2861f7e3e6122e1e0355d58c6f701
Luamix <luizalfonso611@gmail.com>
2024-04-12 15:57:25 -0500

    change logic db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,72 +1,62 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""
+Class DBStorage
+"""
 import os
 from models.base_model import Base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker, scoped_session
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
 from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
-
+classes = [User, Place, State, City, Amenity, Review]
 class DBStorage:
     __engine = None
     __session = None
 
     def __init__(self):
-        user = os.getenv("HBNB_MYSQL_USER")
-        password = os.getenv("HBNB_MYSQL_PWD")
-        host = os.getenv("HBNB_MYSQL_HOST", default="localhost")
-        database = os.getenv("HBNB_MYSQL_DB")
+        mysql_user = os.getenv("HBNB_MYSQL_USER")
+        mysql_pass = os.getenv("HBNB_MYSQL_PWD")
+        mysql_host = os.getenv("HBNB_MYSQL_HOST", default='localhost')
+        mysql_db = os.getenv("HBNB_MYSQL_DB")
 
         self.__engine = create_engine(
-            "mysql+mysqldb://{}:{}@{}/{}".format(user, password, host, database),
-            pool_pre_ping=True,
-        )
+            f"mysql+mysqldb://{mysql_user}:{mysql_pass}@{mysql_host}/{mysql_db}", pool_pre_ping=True)
 
-        Base.metadata.create_all(self.__engine)
+        if os.getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
 
-        Session = sessionmaker(bind=self.__engine)
-        self.__session = Session()
+
+    def all(self, cls=None):
+        new_dict = {}
+        if cls is None:
+            for class_ in classes:
+                instances = self.__session.query(class_).all()
+                for obj in instances:
+                    key = obj.__class__.__name__ + "." + obj.id
+                    new_dict[key] = obj
+        return new_dict
 
     def new(self, obj):
-        with Session(self.__engine):
-            self.__session.add(self.__session)
+        self.__session.add(obj)
 
     def save(self):
-        with Session(self.__engine):
-            self.__session.commit()
+        self.__session.commit()
 
     def delete(self, obj=None):
-        if obj is None:
+        if obj != None:
             self.__session.delete(obj)
 
     def reload(self):
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
-    def all(self, cls=None):
-        new_dict = {}
-
-        if cls is None:
-            classes = [User, Place, State, City, Amenity, Review]
-
-            for c in classes:
-                objs = self.__session.query(c).all()
-
-                for obj in objs:
-                    key = "{}.{}".format(type(obj).__name__, obj.id)
-                    new_dict[key] = obj
-        else:
-            objs = self.__session.query(cls).all()
-
-            for obj in objs:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                new_dict[key] = obj
-        return new_dict
+    def close(self):
+        self.__session.close()
