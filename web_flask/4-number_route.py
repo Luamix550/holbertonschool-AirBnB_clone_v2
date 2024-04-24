dc6f515d3a6753f8480ad60332d3a47f3309bdd9
Luamix <luizalfonso611@gmail.com>
2024-04-24 18:20:53 -0500

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Display "Hello HBNB!" when accessing the root route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display "HBNB" when accessing the /hbnb route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """Display 'C' followed by the value of the text variable"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={"text": "is cool"})
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """Display "Python " when accessing the /python/<text> route """
+    return "Python {}".format(text.replace('_', ' '))
+
+@app.route('/number/<n>', strict_slashes=False)
+def number_route(n):
+    if n.isdigit():
+        return "{} is a number".format(n)
+    else:
+        return
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
