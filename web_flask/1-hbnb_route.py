888a02308b7cb2cf76d40374fa7e3e0d3ed5c707
Luamix <luizalfonso611@gmail.com>
2024-04-24 14:12:19 -0500

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,4 +4,29 @@ script that starts a Flask web application
 """
 from flask import Flask
 
+
 app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """starts a Flask web application
+
+    Returns:
+        str: return a string
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hello_world():
+    """starts a Flask web application
+
+    Returns:
+        str: return a string
+    """
+    return "HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
