61496f02b8191227278f7b78d8f406f8841abebf
Luamix <luizalfonso611@gmail.com>
2024-04-27 18:54:27 -0500

    task 7

--- models/state.py
+++ models/state.py
@@ -3,27 +3,28 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
+from models.city import City
+from os import getenv
+
 
 class State(BaseModel, Base):
     """ State class """
-
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="State", cascade="delete")
 
-    @property
-    def cities(self):
-        """
-        Getter method to return the list of City objects associated with this State.
-        """
-        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-            return self.cities
-        else:
+    # Add condition if storage is db or not
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state",
+                              passive_deletes=True)
+    else:
+        @property
+        def cities(self):
+            """Return the list of City instances with state_id
+            equals to the current State.id"""
             from models import storage
-            from models.city import City
-            city_list = []
-            for city in storage.all(City).values():
+            state_cities = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    state_cities.append(city)
+            return state_cities
