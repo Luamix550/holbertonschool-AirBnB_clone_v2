59ddf278d303e06ec38d4d8a569c5e5db26f4f35
Luamix <luizalfonso611@gmail.com>
2024-04-27 18:54:34 -0500

    task 7

--- console.py
+++ console.py
@@ -63,7 +63,7 @@ class HBNBCommand(cmd.Cmd):
             _cls = pline[: pline.find(".")]
 
             # isolate and validate <command>
-            _cmd = pline[pline.find(".") + 1 : pline.find("(")]
+            _cmd = pline[pline.find(".") + 1 :pline.find("(")]
             if _cmd not in HBNBCommand.dot_cmds:
                 raise Exception
 
